2dd96e983a3790bd1844c4f6e9b690d5ec5d84c4
ogurao <167517023+ogurao@users.noreply.github.com>
2026-06-19 18:31:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+  data() {
+    return {
+      name: "シャチ ぬいぐるみ",
+      price: 1400,
+      description: "丸くて可愛いいシャチのぬいぐるみです。",
+      image: "syati.webp",
+      url: "https://example.com/",
+      stock: 5,
+      priceColor: "#e53935"
+    };
+  }
+}).mount("#app");
